e46049e4b51ab10844b863e001a6e5e68281d2ef
Chris White <cxwembedded@gmail.com>
2023-07-30 19:21:44 -0400

    Copy in Sphinx templates from site-packages

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ release = "0.0.1"
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = "2.1"  # for myst-parser
+needs_sphinx = "2.2"  # for myst-parser, templating
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
